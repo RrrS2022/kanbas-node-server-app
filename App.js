bd558ac9a101e950ea3c5e99cd8ed4b1016d2bf5
RrrS2022 <sun.yuhu@northeastern.edu>
2024-04-04 17:55:07 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Hello from './Hello.js';
+import Lab5 from './Lab5.js';
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+
+import cors from "cors";
+
+// const express = require('express')
+const app = express();
+app.use(cors());
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req,res) => {res.send('Welcome to Full Stack Development!')})
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000)
---
+++ Kanbas/Database/users.js
@@ -0,0 +1,302 @@
+export default [
+    {
+      "_id": "121",
+      "username": "iron_man",
+      "password": "stark123",
+      "firstName": "Tony",
+      "lastName": "Stark",
+      "email": "tony@stark.com",
+      "dob": "1970-05-29",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "122",
+      "username": "dark_knight",
+      "password": "wayne123",
+      "firstName": "Bruce",
+      "lastName": "Wayne",
+      "email": "bruce@wayne.com",
+      "dob": "1972-02-19",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "123",
+      "username": "capt_america",
+      "password": "rogers123",
+      "firstName": "Steve",
+      "lastName": "Rogers",
+      "email": "steve@avengers.com",
+      "dob": "1918-07-04",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "124",
+      "username": "black_widow",
+      "password": "romanoff123",
+      "firstName": "Natasha",
+      "lastName": "Romanoff",
+      "email": "natasha@avengers.com",
+      "dob": "1984-11-22",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "125",
+      "username": "thor_odinson",
+      "password": "mjolnir123",
+      "firstName": "Thor",
+      "lastName": "Odinson",
+      "email": "thor@asgard.com",
+      "dob": "982-05-25",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "126",
+      "username": "hulk_smash",
+      "password": "banner123",
+      "firstName": "Bruce",
+      "lastName": "Banner",
+      "email": "bruce@avengers.com",
+      "dob": "1969-12-18",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "127",
+      "username": "ring_bearer",
+      "password": "shire123",
+      "firstName": "Frodo",
+      "lastName": "Baggins",
+      "email": "frodo@shire.com",
+      "dob": "1368-09-22",
+      "role": "FACULTY"
+    },
+    {
+      "_id": "128",
+      "username": "strider",
+      "password": "aragorn123",
+      "firstName": "Aragorn",
+      "lastName": "Elessar",
+      "email": "aragorn@gondor.com",
+      "dob": "2931-03-01",
+      "role": "FACULTY"
+    },
+    {
+      "_id": "129",
+      "username": "elf_archer",
+      "password": "legolas123",
+      "firstName": "Legolas",
+      "lastName": "Greenleaf",
+      "email": "legolas@mirkwood.com",
+      "dob": "2879-07-15",
+      "role": "FACULTY"
+    },
+    {
+      "_id": "130",
+      "username": "scarlet_witch",
+      "password": "maximoff123",
+      "firstName": "Wanda",
+      "lastName": "Maximoff",
+      "email": "wanda@avengers.com",
+      "dob": "1989-10-02",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "131",
+      "username": "spider_man",
+      "password": "parker123",
+      "firstName": "Peter",
+      "lastName": "Parker",
+      "email": "peter@avengers.com",
+      "dob": "2001-08-10",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "132",
+      "username": "doctor_strange",
+      "password": "strange123",
+      "firstName": "Stephen",
+      "lastName": "Strange",
+      "email": "stephen@avengers.com",
+      "dob": "1960-12-18",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "133",
+      "username": "ant_man",
+      "password": "lang123",
+      "firstName": "Scott",
+      "lastName": "Lang",
+      "email": "scott@avengers.com",
+      "dob": "1979-09-12",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "134",
+      "username": "black_panther",
+      "password": "tchalla123",
+      "firstName": "T'Challa",
+      "lastName": "Wakanda",
+      "email": "tchalla@avengers.com",
+      "dob": "1975-06-27",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "135",
+      "username": "falcon",
+      "password": "wilson123",
+      "firstName": "Sam",
+      "lastName": "Wilson",
+      "email": "sam@avengers.com",
+      "dob": "1980-03-14",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "136",
+      "username": "wolverine",
+      "password": "logan123",
+      "firstName": "Logan",
+      "lastName": "Howlett",
+      "email": "logan@xmen.com",
+      "dob": "1832-05-19",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "137",
+      "username": "storm",
+      "password": "munroe123",
+      "firstName": "Ororo",
+      "lastName": "Munroe",
+      "email": "ororo@xmen.com",
+      "dob": "1950-07-30",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "138",
+      "username": "cyclops",
+      "password": "summers123",
+      "firstName": "Scott",
+      "lastName": "Summers",
+      "email": "scott@xmen.com",
+      "dob": "1963-06-01",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "139",
+      "username": "beast",
+      "password": "mccoy123",
+      "firstName": "Hank",
+      "lastName": "McCoy",
+      "email": "hank@xmen.com",
+      "dob": "1953-11-20",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "140",
+      "username": "groot",
+      "password": "iamgroot123",
+      "firstName": "Groot",
+      "lastName": "",
+      "email": "groot@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "141",
+      "username": "rocket",
+      "password": "rocket123",
+      "firstName": "Rocket",
+      "lastName": "",
+      "email": "rocket@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "142",
+      "username": "gamora",
+      "password": "gamora123",
+      "firstName": "Gamora",
+      "lastName": "",
+      "email": "gamora@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "143",
+      "username": "drax",
+      "password": "drax123",
+      "firstName": "Drax",
+      "lastName": "",
+      "email": "drax@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "144",
+      "username": "star_lord",
+      "password": "starlord123",
+      "firstName": "Star-Lord",
+      "lastName": "",
+      "email": "starlord@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "145",
+      "username": "mantis",
+      "password": "mantis123",
+      "firstName": "Mantis",
+      "lastName": "",
+      "email": "mantis@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "146",
+      "username": "nebula",
+      "password": "nebula123",
+      "firstName": "Nebula",
+      "lastName": "",
+      "email": "nebula@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "147",
+      "username": "yondu",
+      "password": "yondu123",
+      "firstName": "Yondu",
+      "lastName": "",
+      "email": "yondu@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "148",
+      "username": "mantel",
+      "password": "mantel123",
+      "firstName": "Mantel",
+      "lastName": "",
+      "email": "mantel@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "149",
+      "username": "gammora",
+      "password": "gammora123",
+      "firstName": "Gammora",
+      "lastName": "",
+      "email": "gammora@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    },
+    {
+      "_id": "150",
+      "username": "gammora",
+      "password": "gammora123",
+      "firstName": "Gammora",
+      "lastName": "",
+      "email": "gammora@guardians.com",
+      "dob": "Unknown",
+      "role": "STUDENT"
+    }
+  ]
