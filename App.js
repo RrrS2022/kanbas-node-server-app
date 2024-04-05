f61dd3c8332d8acb471be74f1d1de50a5c9230ba
RrrS2022 <sun.yuhu@northeastern.edu>
2024-04-04 23:39:58 -0400

    added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 import cors from "cors";
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
 
 // const express = require('express')
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors());
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 // app.get('/', (req,res) => {res.send('Welcome to Full Stack Development!')})
 app.use(express.json());
+AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
